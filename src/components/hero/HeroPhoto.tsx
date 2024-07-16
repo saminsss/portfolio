a0ce6e94fad756b0a4020d2127ddc55c97982abb
Samin Islam <saminsss@gmail.com>
2024-07-15 22:02:40 -0700

    add border animation

--- src/components/hero/HeroPhoto.tsx
+++ src/components/hero/HeroPhoto.tsx
@@ -15,15 +15,43 @@ const PageTransition = () => {
         animate={{ opacity: 1 }}
         transition={{ delay: 0.3, duration: 0.2, ease: 'easeIn' }}
         className="relative">
-        <div className="w-72 h-72">
+        <div className="w-72 h-72 relative">
           <Image
             src="/assets/photo.jpeg"
             alt="Hero image"
             priority
             quality={100}
             fill
-            className="object-cover border-2 border-secondary rounded-full"
+            className="object-cover rounded-full"
           />
+          <motion.svg
+            width="288"
+            height="288"
+            viewBox="0 0 144 144"
+            className="absolute top-0 left-0 w-full h-full"
+            initial="hidden"
+            animate="visible">
+            <motion.circle
+              cx="72"
+              cy="72"
+              r="71"
+              stroke="#E64833"
+              strokeWidth="2"
+              fill="none"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              initial={{ strokeDasharray: '24 10 0 0' }}
+              animate={{
+                strokeDasharray: ['15 120 25 25', '16 25 92 72', '4 250 22 22'],
+                rotate: [120, 360],
+              }}
+              transition={{
+                duration: 20,
+                repeat: Infinity,
+                repeatType: 'reverse',
+              }}
+            />
+          </motion.svg>
         </div>
       </motion.div>
     </AnimatePresence>
