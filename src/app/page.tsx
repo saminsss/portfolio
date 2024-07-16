b9e23e2e065771e900276f401ea4bb5ba27deed1
Samin Islam <saminsss@gmail.com>
2024-07-15 21:30:06 -0700

    add stats section
    refactor hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,40 +1,11 @@
-import HeroPhoto from '@/components/HeroPhoto';
-import Socials from '@/components/Socials';
-import { Button } from '@/components/ui/button';
-import { FiDownload } from 'react-icons/fi';
-
-const skills = ['AWS', 'Javascript', 'Typescript', 'NodeJS', 'React', 'Vue']
-  .sort()
-  .join(', ');
+import Stats from '@/components/Stats';
+import Hero from '@/components/hero/Hero';
 
 export default function Home() {
   return (
-    <main>
-      <div className="flex flex-col md:flex-row gap-8 items-center justify-between text-center md:text-left">
-        <div className="flex flex-col gap-6 flex-1 order-2 md:order-none">
-          <h4 className="text-secondary">Software Developer</h4>
-          <h1>
-            Hello! I&apos;m <br />{' '}
-            <span className="text-secondary">Samin Islam</span>
-          </h1>
-          <p>
-            I excel at developing full-stack applications on web platforms.{' '}
-            <br />
-            Proficient in <span className="font-semibold">{skills}.</span>
-            <br />
-            Based in{' '}
-            <span className="font-semibold">Vancouver, BC, Canada.</span>
-          </p>
-          <div className="w-72 flex flex-col md:flex-row items-center gap-4 self-center md:self-auto">
-            <Button variant="outline" className="uppercase">
-              <span className="mr-4">Download Resume</span>
-              <FiDownload className="text-lg" />
-            </Button>
-            <Socials iconClass="w-9 h-9 border border-secondary rounded-full flex justify-center items-center text-secondary hover:bg-secondary hover:text-quaternary hover:transition-all duration-200" />
-          </div>
-        </div>
-        <HeroPhoto />
-      </div>
+    <main className="flex flex-col gap-16">
+      <Hero />
+      <Stats />
     </main>
   );
 }
---
+++ src/components/Stats.tsx
@@ -0,0 +1,51 @@
+'use client';
+
+import CountUp from 'react-countup';
+
+const stats = [
+  {
+    num: 3,
+    text: 'Years of experience',
+  },
+  {
+    num: 5,
+    text: 'Projects completed',
+  },
+  {
+    num: 5,
+    text: 'Technologies mastered',
+  },
+  {
+    num: 500,
+    text: 'Code commits',
+  },
+];
+
+const Stats = () => {
+  return (
+    <section>
+      <div className="grid grid-cols-2 lg:grid-cols-4 gap-8 justify-items-start">
+        {stats.map((stat) => (
+          <div
+            key={stat.text}
+            className="flex flex-1 gap-4 items-center justify-center">
+            <CountUp
+              end={stat.num}
+              duration={5}
+              delay={1}
+              className="text-5xl md:text-6xl font-extrabold text-secondary"
+            />
+            <p
+              className={`${
+                stat.text.length < 15 ? 'max-w-28' : 'max-w-36'
+              } leading-snug`}>
+              {stat.text}
+            </p>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Stats;
