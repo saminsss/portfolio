88505b2ead72a6d2e70968ac06dff1fe540520fe
Samin Islam <saminsss@gmail.com>
2024-07-18 12:36:22 -0700

    re-add nav

---
+++ src/components/nav/DesktopNav.tsx
@@ -0,0 +1,43 @@
+'use client';
+
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import { Button } from '../ui/button';
+
+const links = [
+  {
+    name: 'home',
+    path: '/',
+  },
+  {
+    name: 'work',
+    path: '/work',
+  },
+];
+
+const DesktopNav = () => {
+  const path = usePathname();
+  return (
+    <nav className="flex gap-8 items-center capitalize transition-all">
+      {links.map((link) => {
+        return (
+          <Link
+            href={link.path}
+            key={link.path}
+            className={
+              path === link.path
+                ? 'text-secondary border-b-2 border-secondary font-medium '
+                : ''
+            }>
+            {link.name}
+          </Link>
+        );
+      })}
+      <Link href="/resume">
+        <Button>Resume</Button>
+      </Link>
+    </nav>
+  );
+};
+
+export default DesktopNav;
---
+++ src/components/nav/Logo.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+
+const Logo = () => {
+  return (
+    <Link href="/">
+      <h3>
+        samin<span className="text-secondary">.</span>
+      </h3>
+    </Link>
+  );
+};
+
+export default Logo;
